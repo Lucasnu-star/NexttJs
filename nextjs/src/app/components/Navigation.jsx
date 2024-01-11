26d7bc14dc8eb1120d028d904649ffc6fcbf8052
Lucas Gabriel Nuñez <65827279+Lucasnu-star@users.noreply.github.com>
2024-01-10 21:16:51 -0300

    ,

--- nextjs/src/app/components/Navigation.jsx
+++ nextjs/src/app/components/Navigation.jsx
@@ -2,54 +2,43 @@ import React from "react";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button} from "@nextui-org/react";
 
 
-
-const links= [{
-    label:'Home',
-    route:'/'
-  },
-  {
-    label:'Menu',
-    route:'/Menu'
-  
-  },
-{
-    label:'Promociones',
-    route:'/Promociones'
-}] 
-
-
-function Navigation() {
+export default function App() {
   return (
     <Navbar>
-    <NavbarBrand>
-      
-      <p className="font-bold text-inherit">ACME</p>
-    </NavbarBrand>
-    <NavbarContent className="hidden sm:flex gap-4" justify="center">
-      <NavbarItem>
-      {links.map(({ label, route }) => (
-                <li key={route}>
-                    <Link color="foreground" href="#">
-          {label}
-        </Link>
-                </li>
-            ))}
-        
-      </NavbarItem>
-    </NavbarContent>
-    <NavbarContent justify="end">
-      <NavbarItem className="hidden lg:flex">
-        <Link href="#">Login</Link>
-      </NavbarItem>
-      <NavbarItem>
-        <Button as={Link} color="primary" href="#" variant="flat">
-          Sign Up
-        </Button>
-      </NavbarItem>
-    </NavbarContent>
-  </Navbar>
-)
+      <NavbarBrand>
+       
+        <p className="font-bold text-inherit">ACME</p>
+      </NavbarBrand>
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link color="primary" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link href="#" aria-current="page">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="primary" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="primary" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+    </Navbar>
+  );
 }
-export default Navigation
+
 
 
